731b23c4a8ce8a2e367076eb46e2890f22d52a2c
Techguyy007 <150214440+Techguyy007@users.noreply.github.com>
2024-05-12 14:45:16 -0500

    Add files via upload

---
+++ my_python_journey.py
@@ -0,0 +1,20 @@
+print('Hello world!')
+message = "Hello World"
+print (message)
+
+message = ("Hello Python Crash Course World!")
+print (message)
+mesage = "Hello Pyhton Crash Course Reader"
+print (mesage)
+#changing case - capitalization
+name = "richmond boakye"
+print (name.title())
+#changing case to lower letters 
+print (name.lower()) 
+#changing case to upper letters
+print (name.upper())
+#inserting value of variables using f strings
+first_name = "richmond"
+last_name = "danquah"
+full_name = f" {first_name}   {last_name}" 
+print (full_name.title())
